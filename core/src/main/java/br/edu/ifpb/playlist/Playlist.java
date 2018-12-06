ac82b27bf227c035a33ccdddbe9862b0c619537d
Fernanda Vieira <fernanda.silva_sj@hotmail.com>
2018-12-06 10:21:51 -0300

    adicionandoplaylist

--- core/src/main/java/br/edu/ifpb/playlist/Playlist.java
+++ core/src/main/java/br/edu/ifpb/playlist/Playlist.java
@@ -1,9 +1,31 @@
 package br.edu.ifpb.playlist;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+
 /**
  *
  * @author fernanda
  */
+
+@Stateful
 public class Playlist {
     
+    @EJB
+    private List<Banda> bandas = new CopyOnWriteArrayList<>();
+    
+    public void novaBanda(Banda banda){
+        this.bandas.add(banda);
+    }
+    
+    public void removerBanda(Banda banda) {
+        this.bandas.remove(banda);
+    }
+
+    public List<String> listarBandas() {
+        return Collections.unmodifiableList(bandas);
+    }
 }
